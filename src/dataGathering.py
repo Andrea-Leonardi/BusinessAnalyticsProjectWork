d4d0c51690b5c8fbc672026bb30a0d430e1823e3
Andrea Leonardi <leoandrea14a@gmail.com>
2026-03-15 16:08:45 +0100

    Update dataGathering.py

--- src/dataGathering.py
+++ src/dataGathering.py
@@ -26,7 +26,7 @@ except FileNotFoundError:
 # %%
 # Define the period for which to download data (last 2 years)
 end_date = datetime.now()
-start_date = pd.Timestamp(end_date - timedelta(days=365*2))
+start_date = pd.Timestamp(end_date - timedelta(days=365*5))
 
 # create a dictionary to store the dataframes for each company
 company_dfs = {}
@@ -35,7 +35,7 @@ for ticker in df["Ticker"]:
     print(f"download data for {ticker}...")
     # download the historical data for the ticker (monthly data with auto_adjust)
     stock = yf.Ticker(ticker)
-    data = stock.history(period='2y', interval='1mo', auto_adjust=True)
+    data = stock.history(period='5y', interval='1mo', auto_adjust=True)
     if not data.empty:
         # 'Close' is the adjusted close price
         adj_close = data['Close']
@@ -52,12 +52,20 @@ for ticker in df["Ticker"]:
         print(f"no data available for {ticker}")
 
 # Now company_dfs contains a DataFrame for each ticker with the adjusted close prices
-# Example: to access AAPL data
-print(company_dfs['AAPL'].head())
+# Example access AAPL data
+aapl = company_dfs['AAPL']
 # %%
+#plot the data for each company different colors for each company standardize the data to make it comparable (divide by the first value) and plot the adjusted close prices over time
+import matplotlib.pyplot as plt
+plt.figure(figsize=(12, 6))
+for ticker, df in company_dfs.items():
+    standardized_prices = df['Adj Close'] / df['Adj Close'].iloc[0]  # Standardize by the first value
+    plt.plot(standardized_prices.index, standardized_prices.values, label=ticker)   
+plt.title('Standardized Adjusted Close Prices Over Time')
+plt.xlabel('Date')
+plt.ylabel('Standardized Price')
+plt.legend()
+plt.grid()
+plt.show()
 
-# Now company_dfs contains a DataFrame for each ticker with the adjusted close prices
-# Example: to access AAPL data
-# print(company_dfs['AAPL'].head())
 # %%
-
