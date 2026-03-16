eb10d6f29c370f78b27ec4ab3875e5a9004ec3a3
Andrea Leonardi <leoandrea14a@gmail.com>
2026-03-16 15:09:12 +0100

    Update config.py

--- src/config.py
+++ src/config.py
@@ -4,4 +4,4 @@ from pathlib import Path
 
 ROOT = Path.cwd().parent
 DATA = ROOT / "data"
-TOP200 = DATA / "possible_enterprises" / "top_200_enterprises.csv"
+ENT = DATA / "possible_enterprises" / "enterprises.csv"
