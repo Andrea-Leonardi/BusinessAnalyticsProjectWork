0b02dfba304614f370c9f79373a1c7981e89af6a
Andrea Leonardi <leoandrea14a@gmail.com>
2026-03-17 15:12:31 +0100

    moved SEC files

--- src/valutazione_dati.py
+++ src/valutazione_dati.py
@@ -3,7 +3,7 @@ import config as cfg
 
 sec_dataset = pd.read_csv(cfg.SEC_DATASET)
 
-print(sec_dataset.head(1))
+print(sec_dataset.head(10))
 
 valori_nulli = sec_dataset.isna().sum()
 print(valori_nulli)
