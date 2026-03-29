5c590ce46dd507340ed75ebdcadd4fd7d7a09c57
Giuse13 <153390547+Giuse13@users.noreply.github.com>
2026-03-29 16:35:55 +0200

    Add validation.py and update training_model.py for LASSO logistic regression
    Test variable selection procedure (convergence issues) and refactor training_model.py to integrate validation workflow

--- src/modeling/classic_ML_model/lasso_model/training_model.py
+++ src/modeling/classic_ML_model/lasso_model/training_model.py
@@ -3,35 +3,17 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 
 
-import numpy as np
-from sklearn.model_selection import GridSearchCV
 
 
 
-param_grid = {
-    "C": np.arange(0.1, 1.1, 0.1)   #lambda non usabile;   C = 1/lambda
-}
-    
-
-models = {}
-
-for C in param_grid["C"]:
-    model = Pipeline([
-        ("scaler", StandardScaler()),
-        ("model", LogisticRegression(
-            penalty="l1",
-            solver="saga",
-            C=C,
-            max_iter=5000,
-            random_state=42
-        ))
-    ])
-
-    models[C] = model
+pipeline = Pipeline([
+    ("scaler", StandardScaler()),
+    ("model", LogisticRegression(
+        penalty="l1",
+        solver="saga",
+        max_iter=5000,
+        random_state=42
+    ))
+])
 
 
-
-print(models)
-
-
-# lasso_modello = lasso_logit.fit(X_train, y_train)  #non accetta valori nulli !!!
---
+++ src/modeling/classic_ML_model/lasso_model/validation.py
@@ -0,0 +1,43 @@
+import numpy as np
+from sklearn.model_selection import GridSearchCV
+
+from training_model import pipeline
+
+from pathlib import Path
+import sys
+
+sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
+
+from split_data import X_train, y_train, X_validation, y_validation
+from sklearn.metrics import balanced_accuracy_score
+
+
+
+
+param_grid = {
+    "model__C": np.arange(0.1, 1.1, 0.1)
+}
+
+best_score = -1
+best_C = None
+best_model = None
+
+"""
+for C in param_grid["model__C"]:
+
+    pipeline.set_params(model__C=C)
+
+    pipeline.fit(X_train, y_train)
+
+    y_pred = pipeline.predict(X_validation)
+
+    score = balanced_accuracy_score(y_validation, y_pred)
+
+    if score > best_score:
+        best_score = score
+        best_C = C
+        best_model = pipeline
+
+"""
+
+print(best_C, best_score)
