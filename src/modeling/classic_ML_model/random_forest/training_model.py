18e05756b9e90caa3e931d69e3ad919e64c02945
Giuse13 <153390547+Giuse13@users.noreply.github.com>
2026-04-09 18:15:22 +0200

    Add performance.py for Random Forest test evaluation and fix minor training bug
    Implement accuracy evaluation on test set and correct small logical issue in training_model.py

--- src/modeling/classic_ML_model/random_forest/training_model.py
+++ src/modeling/classic_ML_model/random_forest/training_model.py
@@ -7,7 +7,7 @@ sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
 
 from split_data import X_train_full, y_train_full
 
-from validation import best_params, selected_variables
+from validation import best_params, selected_variables, X_train_selected, X_validation_selected 
 
 
 #inizializzazione modello con i migliori iperparametri trovati
@@ -18,6 +18,8 @@ random_forest_model = RandomForestClassifier(
     n_jobs=-1,
 )
 
+#adattamento covariate set alle variabili scelte
+X_train_selected_full = X_train_full[selected_variables]
 
 #training
-random_forest_model.fit(X_train_full, y_train_full)
+random_forest_model.fit(X_train_selected_full, y_train_full)
